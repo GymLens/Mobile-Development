b71920958046462d0a8f91a357cda2de91bf3db6
rarasuryap <152159553+rarasuryap@users.noreply.github.com>
2024-11-23 14:25:08 +0700

    hapus walk_run2

--- app/src/main/java/com/example/capstoneprojectmd/ui/WalkRun/WalkRunFragment.kt
+++ app/src/main/java/com/example/capstoneprojectmd/ui/WalkRun/WalkRunFragment.kt
@@ -35,7 +35,7 @@ class WalkRunFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_walk_run2, container, false)
+        return inflater.inflate(R.layout.fragment_walk_run, container, false)
     }
 
     companion object {
