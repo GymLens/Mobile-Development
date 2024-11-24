1d7d79b39d97868eb9e6f000f587281001d83a49
putriamelia16 <139982189+putriamelia16@users.noreply.github.com>
2024-11-24 16:40:14 +0700

    nnnn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.capstoneprojectmd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.capstoneprojectmd"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -52,7 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
     implementation ("androidx.activity:activity-ktx:1.5.0")
     implementation ("androidx.fragment:fragment-ktx:1.5.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
--- app/src/main/java/com/example/capstoneprojectmd/data/retrofit/ApiService.kt
+++
@@ -1,4 +0,0 @@
-package com.example.capstoneprojectmd.data.retrofit
-
-class ApiService {
-}
